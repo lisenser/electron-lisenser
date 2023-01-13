2ea99f2aa1dab64eeb1ef2b8326ff50a5d87ee54
ifedapoolarewaju <ifedapoolarewaju@gmail.com>
2023-01-13 18:16:20 +0100

    fix packager test

--- test-build.js
+++ test-build.js
@@ -1,11 +1,7 @@
 const assert = require("assert");
 const fs = require("fs");
-const { getLicenseStatus, getTrialStatus, startTrial, activateLicenseKey, Lisenser } = require("./dist/main");
+const { Lisenser } = require("./dist/main");
 
-assert.equal(typeof getLicenseStatus, "function");
-assert.equal(typeof getTrialStatus, "function");
-assert.equal(typeof startTrial, "function");
-assert.equal(typeof activateLicenseKey, "function");
 assert.equal(typeof Lisenser, "function");
 assert.equal(fs.existsSync("./dist/renderer/activate.html"), true);
 assert.equal(fs.existsSync("./dist/renderer/activate.js"), true);
